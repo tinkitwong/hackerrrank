279445ea6a11806160b31445107757ea3b5754dc
tinkitwong <tinkitwong@gmail.com>
2022-03-16 03:58:40 +0800

    Update Javascript Progress

---
+++ 10DaysOfJS/createRectangleObject.js
@@ -0,0 +1,20 @@
+function Rectangle(a, b) {
+    return {
+        length : a,
+        width : b,
+        perimeter : 2*a+2*b,
+        area : a*b
+    }
+}
+
+/**
+ * Input
+ * 4
+ * 5
+ * 
+ * Output
+ * 4
+ * 5
+ * 18
+ * 20
+ */
