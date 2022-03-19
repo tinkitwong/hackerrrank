78e44c2a115acb2a00109e65bc567e8f95a20ec7
tinkitwong <tinkitwong@gmail.com>
2022-03-19 20:09:56 +0800

    Update JS Progress

---
+++ 10DaysOfJS/regularExpIII.js
@@ -0,0 +1,13 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Character_Classes
+    const re = /\d+/g; 
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
