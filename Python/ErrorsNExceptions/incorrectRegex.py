238f09bef5a48e5b0c5e0b1406dd96f5ad647011
tinkitwong <tinkitwong@gmail.com>
2022-03-23 18:33:09 +0800

    Update Python Hackerrank Progress

---
+++ Python/ErrorsNExceptions/incorrectRegex.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+for _ in range(int(input())):
+    try:
+        re.compile(input())
+        print(True)
+    except re.error:
+        print(False)
+
+
